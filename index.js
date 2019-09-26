230c3dde7aadfbc2922516a8913b1b6f2698417b
Michael Hibbard <veg.it.up@live.com>
2019-09-26 00:12:05 -0700

    format rework, add imgs, style imprvmts

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const http      = require("http");
 const express   = require("express");
+const server    = express();
+const http      = require("http");
+const https     = require("https");
 const path      = require("path");
+const fs        = require("fs");
 const PORT      = 3001;
 const hostname  = '127.0.0.1';
-const server    = http.createServer(express());
+const creds     = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+    passphrase: "8675309"
+};
+http.createServer(server)
+    .listen(PORT, hostname, (req, res) => {
+        console.log(`Mike is Listening at http://${hostname}:${PORT}/`);
+    })
+https.createServer(creds, server)
+    .listen(8443, (req, res) => {
+        console.log(`Mike secure server is running on https://localhost:${PORT} or 8443/ `);
+    })
 
-express()
+server
     //App Config
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
@@ -16,7 +31,3 @@ express()
     .get('/about', (req, res) => res.render('about'))
     .get('/contact', (req, res) => res.render('contact'))
     .get('/resume', (req, res) => res.sendFile('public/documents/Hibbard WebDev Resume.pdf', { root : __dirname}))
-
-    .listen(PORT, hostname, (req, res) => {
-        console.log(`Mike is Listening at http://${hostname}:${PORT}/`)
-    })
